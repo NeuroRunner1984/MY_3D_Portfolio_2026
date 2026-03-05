39999ae79ace23d7df30ed9699feab2637972b3e
Rich Greene <157834945+RICHelDIOS@users.noreply.github.com>
2026-03-05 12:31:18 -0500

    Complete Changes! Final demo.

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,5 +1,6 @@
-import { OrbitingCircles } from "./OrbitingCircles";
 
+import{OrbitingCircles} from "./OrbitingCircles";
+ 
 export function Frameworks() {
   const skills = [
     "auth0",
@@ -21,16 +22,14 @@ export function Frameworks() {
   ];
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
-      <OrbitingCircles iconSize={40}>
-        {skills.map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
-        ))}
+      <OrbitingCircles iconSize={40}> 
+        {skills.map((skill, index) => (<Icon key={index} src={`assets/logos/${skill}.svg`} />))}
       </OrbitingCircles>
-      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
-        {skills.reverse().map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
-        ))}
-      </OrbitingCircles>
+        
+      <OrbitingCircles 
+      iconSize={25} radius={100} reverse speed={2}>
+         {skills.reverse().map((skill, index) => (<Icon key={index} src={`assets/logos/${skill}.svg`} />))}
+       </OrbitingCircles>
     </div>
   );
 }
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,22 +1,28 @@
-import { mySocials } from "../constants";
+import { a } from "motion/react-client";
+import { mySocials } from "../constants"
+
+
 const Footer = () => {
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
-      <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
-      <div className="flex gap-2">
-        <p>Terms & Conditions</p>
-        <p>|</p>
-        <p>Privacy Policy</p>
+   <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+    <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full">
+    <p className="flex gap-2">
+      Terms & Conditions
+    </p>
+    <p>|</p>
+    <p>Privacy Policy</p>
+    </div>
+    <div className="flex gap-3">
+      {mySocials.map((social, index) =>
+      ( <a href={social.href} key={index}>
+        <img src={social.icon} className="w-5 h-5" alt={social.name} />
+      </a>
+
+      )
+      )}
       </div>
-      <div className="flex gap-3">
-        {mySocials.map((social, index) => (
-          <a href={social.href} key={index}>
-            <img src={social.icon} className="w-5 h-5" alt={social.name} />
-          </a>
-        ))}
-      </div>
-      <p>© 2025 Ali. All rights reserved.</p>
-    </section>
+      <p>© 2025 Rich Greene. All rights reserved.</p>
+   </section>
   );
 };
 
